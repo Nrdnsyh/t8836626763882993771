13fb6861dc73bd1c67cc883bb831f2b6096dacde
Nrdnsyh <88859005+Nrdnsyh@users.noreply.github.com>
2021-11-16 09:11:55 +0700

    Add files via upload

---
+++ Messages.py
@@ -0,0 +1,37 @@
+m_start = "Welcome to Anonymous Chat\n\n💬 to find chat friends use the New Chat button"
+
+m_is_not_free_users = (
+    "❗️ Sorry you are not in chat"
+)
+
+m_is_connect = "✅ You are already in the chat, please send your chat"
+
+m_play_again = "Do you want to chat with other people?"
+
+m_is_not_user_name = "❌ Sorry, our bot can only communicate if you have a username"
+
+m_good_bye = "❌ Sorry your interlocutor left the chat"
+
+m_disconnect_user = "Sorry, the connection was lost!"
+
+m_failed = "❌ Loss happened"
+
+m_like = "✅ You send the username to the other person"
+
+m_dislike_user = "Chat Has Ended"
+
+m_dislike_user_to = (
+    "⛔ Your interlocutor leaves the chat"
+)
+
+m_send_some_messages = "The bot could not continue the pan from the bot"
+
+m_has_not_dialog = "You are not in chat"
+
+dislike_str = "⛔ Exit"
+
+like_str = "📠 Send Contact"
+
+
+def m_all_like(x):
+    return "My contact " + "username : @" + str(x)
